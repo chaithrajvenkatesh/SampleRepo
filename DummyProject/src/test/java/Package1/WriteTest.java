49efefd8f115c6669fd0bec7a9a20f8c24aca300
Admin <Admin@LAPTOP-N50CKJOD>
2025-04-06 18:23:25 +0530

    Sample first commit

---
+++ DummyProject/src/test/java/Package1/WriteTest.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class WriteTest {
+
+	@Test
+	public void readTest() {
+		System.out.println("Read test executed Successfully");
+	}
+}
